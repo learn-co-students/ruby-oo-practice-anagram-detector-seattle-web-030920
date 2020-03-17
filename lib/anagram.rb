199cc15bb96553ecbd716f2ca7867d805ee337a8
Roger Robertson Jr <rjrjr@pm.me>
2020-03-17 13:11:47 -0700

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+require 'pry'
+class Anagram
+
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+    
+    def match(other_word_array)
+        other_word_array.map {|other_word| other_word.split("").sort == @word.split("").sort ? other_word : nil}.compact
+    end
+    #binding.pry
+end
